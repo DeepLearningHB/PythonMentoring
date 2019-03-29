9d2c42786bc48384ae30567fbdbbb9e6df0ae9ed
DeepLearningHB <37934236+DeepLearningHB@users.noreply.github.com>
2019-03-29 16:18:46 +0900

    Update 20195161_문현승_Lab01_Task03.py

--- PythonMentoring_OT/20195161_문현승-Lab01/20195161_문현승_Lab01_Task03.py
+++ PythonMentoring_OT/20195161_문현승-Lab01/20195161_문현승_Lab01_Task03.py
@@ -1,19 +1,22 @@
-3�� ����
-
-l=['����', '����', '�뱸', '�λ�', '���', '��õ']
-
-i=len(l)
-
-type(i)
-
-j=int(i)
 
 
+# l=['서울', '대전', '대구', '부산', '울산', '인천']
+# i=len(l)
 
-for kk in range(j):
+# type(i) #타입 체크 good
 
-  a="���� "
+# j=int(i) #불필요한 형 변환입니다.
 
-  b="��� ����Դϴ�"
 
-  print(a+l[0+kk]+b)
+
+# for kk in range(j):
+
+#   a="나는 "
+
+#   b="사는 사람입니다"
+
+#   print(a+l[0+kk]+b)
+
+local_list = ['서울', '대전', '대구', '부산', '울산', '인천']
+for i in local_list:
+  print("나는", i,"사는 사람입니다.") #코드가 많이 간략화되었죠?
