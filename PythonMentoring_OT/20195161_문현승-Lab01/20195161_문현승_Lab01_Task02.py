cbbf15bfba0fce86371d08acc4c71362053bd05c
DeepLearningHB <37934236+DeepLearningHB@users.noreply.github.com>
2019-03-29 16:16:29 +0900

    Update 20195161_문현승_Lab01_Task02.py

--- PythonMentoring_OT/20195161_문현승-Lab01/20195161_문현승_Lab01_Task02.py
+++ PythonMentoring_OT/20195161_문현승-Lab01/20195161_문현승_Lab01_Task02.py
@@ -1,13 +1,7 @@
-2�� ����
-
+#과제는 1부터 더하는 내용이였습니다.
+#사소할 수 있지만 문제의 요구조건에 맞춰주는게 좋아요!
 a=int(input())
 sum=0
-
-
-
-for i in range(0,a+1,1):
-
-
-  sum=sum+i
-
+for i in range(1, a+1): # range의 자동 incease는 1입니다. 생략해도 됩니다. 
+    sum+=i # += 연산자가 있다는 것을 기억해주세요!
 print(sum)
