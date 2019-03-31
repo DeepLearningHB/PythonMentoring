4cbb4b70f56dae09b0491cf9cf8f632d86ecccde
DeepLearningHB <37934236+DeepLearningHB@users.noreply.github.com>
2019-03-31 19:15:43 +0900

    Update 20155176_황찬우_Lab01_Task02.py

--- PythonMentoring_OT/20155176_황찬우_Lab01/20155176_황찬우_Lab01_Task02.py
+++ PythonMentoring_OT/20155176_황찬우_Lab01/20155176_황찬우_Lab01_Task02.py
@@ -2,10 +2,10 @@ sum = 0
 
 user = input("더해줄 마지막 수를 입력하세요 : ")
 
-if(user.isdigit() == True) :
+#if(user.isdigit() == True) :
+if user.isdigit(): #다양한 입력 변수에 대한 처리 Good / user.isdigit() 만 써도 됩니다!
     for i in range(1, int(user) + 1) :
         sum += i
-
     print(sum)
 
 else :
