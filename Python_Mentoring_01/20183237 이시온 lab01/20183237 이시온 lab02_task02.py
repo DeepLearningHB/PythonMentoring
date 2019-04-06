caf7873886764980f4bca99403947d592329be89
DeepLearningHB <37934236+DeepLearningHB@users.noreply.github.com>
2019-04-06 15:45:12 +0900

    Update 20183237 이시온 lab02_task02.py

--- Python_Mentoring_01/20183237 이시온 lab01/20183237 이시온 lab02_task02.py
+++ Python_Mentoring_01/20183237 이시온 lab01/20183237 이시온 lab02_task02.py
@@ -1,9 +1,11 @@
 #2
 res = int(input("시험 점수 : "))
+#잘못된 입력 시에는 지속적으로 입력을 받아야합니다.
 
-if res > 100 or res < 0 :
-    print(res = int(input("시험 점수 : ")))
-elif res >= 90 :
+while res > 100 or res < 0 :
+   # print(res = int(input("시험 점수 : "))) #변수를 입력받고자 할 때는
+    res = int(input("시험 점수 : ")) #와 같은 형태로 입력받아야 합니다.
+if res >= 90 :
     print("A")
 elif res >= 80 :
     print("B")
